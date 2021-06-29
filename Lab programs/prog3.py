c1f22e1a71792204fb7fcc473a25cd5b4c99964a
Chandan-CV <Chandan-CV@users.noreply.github.com>
2021-06-29 18:18:34 +0530

    added the prevoius lab programs

---
+++ Lab programs/prog3.py
@@ -0,0 +1,17 @@
+# WAP to accept lower limit and upper limit and print the even and odd numbers between those
+
+def oddEven(ll,ul):
+    odd =[]
+    even = []
+    for x in range (ll,ul):
+        if (x % 2==0):
+            even.append(x)
+        else:
+            odd.append(x)
+    return even,odd
+
+ll = int(input("enter the lower limit"))
+ul = int(input("enter the upper limit"))
+even, odd = oddEven(ll,ul)
+print("even = ",  even)
+print ("odd = ", odd)
---
+++ Lab programs/prog4.py
@@ -0,0 +1,7 @@
+# accept 2 string and concatinate them
+
+def con(s1, s2):
+    return (s1+s2)
+s1 = input("enter the string 1")
+s2 = input("enter the string 2")
+print( con(s1,s2))
