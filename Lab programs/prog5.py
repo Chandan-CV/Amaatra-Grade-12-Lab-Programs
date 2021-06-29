d56d7492a443f75220109a821953627fcfdf09bd
Chandan-CV <Chandan-CV@users.noreply.github.com>
2021-06-29 18:18:34 +0530

    added the prevoius lab programs

---
+++ Lab programs/prog5.py
@@ -0,0 +1,13 @@
+# Wap user def func to perform all arithmatic op on 2 numbers
+
+def op(a,b):
+    print (a,"+",b,"=",a+b)
+    print (a,"-",b,"=",a-b)
+    print (a,"/",b,"=",a/b)
+    print (a,"*",b,"=",a*b)
+    print (a,"%",b,"=",a%b)
+
+a = int(input("enter number 1"))
+b = int(input("enter number 2"))
+print (op(a,b))
+
