5bf5ac00a7de484c015a25066e5c2df1bd4ac929
Chandan <chandancvp08@gmail.com>
2021-06-30 15:38:56 +0530

    changed the error in Lab program, as pointed out by shriyans

--- Lab programs/prog1.py
+++ Lab programs/prog1.py
@@ -3,11 +3,11 @@
 def neg():
     a= int(input("enter the first number"))
     b= int(input("enter the first number"))
-    print ("result =" , a**2 +b**2 - 2*a*b)
+    print ("result =" , a**3 - (3*(a**2)*b) + (3*a*(b**2)) - b**3)
 def pos():
     a= int(input("enter the first number"))
     b= int(input("enter the first number"))
-    print ("result =" , a**2 +b**2 + 2*a*b)
+    print ("result =" , a**3 + (3*(a**2)*b) + (3*a*(b**2)) + b**3)
 
 choice = int (input ("enter 1 for (a-b)^3 \n  enter 2 for (a+b)^3"))
 if (choice==1):
