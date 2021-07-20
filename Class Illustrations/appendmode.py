58fccd6949480265769f7c881bf2f40ca23feefa
Chandan <chandancvp08@gmail.com>
2021-07-20 14:41:12 +0530

    added some more info on the path

---
+++ Class Illustrations/appendmode.py
@@ -0,0 +1,2 @@
+f = open("Class Illustrations/append.txt","a") # note I use linux, so I need to use a forward slash, for windows its backlash
+f.writelines("\n yo this should be at the end of the file")
--- Class Illustrations/writelinesFunction.py
+++ Class Illustrations/writelinesFunction.py
@@ -1,4 +1,4 @@
-f= open("Class Illustrations/writeline.txt", "w")
+f= open("Class Illustrations/writeline.txt", "w") # note I use linux, so I need to use a forward slash, for windows its backlash
 l = ["hellooo ", "woah ", "this is ", " really cool ", "ta daaaaaa"] # it even works with tuple
 f.writelines(l)
 f.close()
