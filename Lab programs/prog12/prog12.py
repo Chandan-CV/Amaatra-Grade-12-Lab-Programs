5e035ba35b5e6d310148ceee99bc2ad74f24f49b
Chandan <chandancvp08@gmail.com>
2021-10-03 13:54:03 +0530

    added program 12 from shriyans

--- Lab programs/prog12/prog12.py
+++ Lab programs/prog12/prog12.py
@@ -1,7 +1,16 @@
-r = open("Lab programs/prog12/provider.txt","r") # \ in windows
-w = open("Lab programs/prog12/target.txt","w") # \ in windows
+# Name: Shriyans Gandhi
+# Class:12
+# Date of executiom: 29/6/21
+# Write a Program to read a file named "abc.txt" and copy these contents into another file name "xyz.txt"
 
 def move():
-    w.write(r.read())
-
+    f=open("Text Files/Lab Program 12/abc.txt","r")
+    f_copy=open("Text Files/Lab program 12/xyz.txt","w")
+    while True:
+        c=f.read(1)
+        if not c:
+            break
+        f_copy.write(c)
+    f.close()
+    f_copy.close()
 move()
