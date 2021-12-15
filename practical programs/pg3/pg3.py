1e1e9e46536841849b3cbd31aae426c0d44fd3b6
Chandan <chandancvp08@gmail.com>
2021-12-15 11:31:48 +0530

    corred the issues pointed out by aditya yogesh

--- practical programs/pg3/pg3.py
+++ practical programs/pg3/pg3.py
@@ -1,16 +1,20 @@
 # Remove all the lines that contain the character 'a' in a file
 # and write it to another file.
 
-f = open("practical programs/pg3/file.txt")
+f = open("practical programs/pg3/file.txt", "r+")
 
 f2 = open("practical programs/pg3/targetfile.txt","w")
 a = f.readlines()
+a1=a
 af=[]
 for x in range(0,len(a)):
-    if ("a" not in a[x]):
+    if ("a" in a[x]):         
         af.append(a[x])
-            
+        a1[x]=""             # replaces 'a' with empty string
+f.seek(0)
 f2.writelines(af)
+f = open("practical programs/pg3/file.txt", "w")
+f.writelines(a1)           # writes edited text on original string
 
 
 
