e334e2f2952ef1b3e1a87ed81200f41dc90f4df5
Chandan <chandancvp08@gmail.com>
2021-12-20 09:09:05 +0530

    sat revision

--- aDayBeforeExamPractice.py
+++ aDayBeforeExamPractice.py
@@ -1,3 +1,50 @@
-import pickle
-f = open("practical programs/pg5/data.dat","rb")
-print(pickle.load(f))
+import math
+sum(1,2,3,5) # this will give an error
+sum ([1,232,432,324,32,4,2]) # this will not give an error
+
+print(-True -True --True) # -1 -1--1 => -2+1
+
+f.seek(4) # the default value of the second var is 0, and this will make bring the curzor to pos 4
+
+math.randint() # used to generate random integer values
+math.random() #used to generate values from 0.0 to 1
+
+a=10
+def lol():
+    global a # the changes in the val of a, reflects globally throught the program
+l1 =[1,2,3,4,5,6]
+l1.append( len(l1)) # this will add the length of the list l1 at the end of the list
+
+
+# remove() function will take the value and not the index, and it will remove the first occurance of the number
+
+
+
+#append function will take only one input
+
+
+function # named group of instructions that accomplish a specific task when it is invoked
+
+
+# default arguments can be skipped in a function call
+
+f.writelines() # takes a list of strings as an argument and writes it to the text file
+
+f.readlines() # will return a list
+
+math.randint(1,3) # this will return a number between 1,2, and 3
+
+
+# you can write file mode in any of the following formats
+# wb+ , w+b, and +wb
+
+#wb+ ==> write and read (file will be over written)
+# rb+ ==> read and write (file will not be overwritten)
+
+
+# if you try to change the value of string, you will get a type error
+
+s = "True"
+r = "False"
+
+bool(r) == bool(s) # this condition will return the value true
